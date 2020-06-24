8a4dbede92e42b2abf6029e00004a7a8b93f6995
Dylan Weber <djweber3@en4181828l.fulton.ad.asu.edu>
2020-06-23 18:58:20 -0700

    debugging

--- cleverrx_bot/scripts/new_bot_launcher.py
+++ cleverrx_bot/scripts/new_bot_launcher.py
@@ -146,57 +146,7 @@ def train_batch_of_gpt2_models(preprocessor, parameter_dict, results_dir = resul
         plt.savefig(results_dir + '/' + parameter_dict['filename'] +'/'+'loss_plot.png')
 
 
-def train_batch_of_bert_models(preprocessor, parameter_dict, results_dir = results_dir, model_storage_dir = model_storage_dir, type = 'keyword', file_stem = file_stem, medium = True):
-    datasets = preprocessor.prepared_datasets
 
-    for dataset_name in datasets.keys():
-        print("starting to train model on" + dataset_name)
-
-        model =  model_dict[type].from_encoder_decoder_pretrained('bert-base-uncased', 'bert-base-uncased')
-
-        preprocessor.set_active_dataset(dataset_name)
-
-        parameter_dict['filename'] = file_stem + '_' + type + '_' + dataset_name
-        results_path = Path(Path(results_dir)/Path(parameter_dict['filename']))
-        model_path = Path(Path(model_storage_dir)/Path(parameter_dict['filename']))
-        results_path.mkdir(parents = True, exist_ok = True)
-        model_path.mkdir(parents = True, exist_ok = True)
-
-        preprocessor.set_get_type('prepend_space')
-        trained_model, optimizer, scheduler, loss_data = butils.train_hugging_encode_decode(dataset,
-                                                                          parameter_dict['epochs'],
-                                                                          parameter_dict['num_worker'],
-                                                                          parameter_dict['batch_size'],
-                                                                          parameter_dict['learning_rate'],
-                                                                          parameter_dict['weight_decay'],
-                                                                          parameter_dict['eps'],
-                                                                          parameter_dict['warmup_steps'],
-                                                                          model,
-                                                                          collate_fn = preprocessor.collate_fn
-                                                                          )
-        #saving transformers stuff - this can all be loaded again using (transformer_object).from_pretrained(model_storage_dir+'/'+parameter_dict['filename'])
-        preprocessor.active_dataset.to_csv(results_path/'training_data.csv')
-        trained_model.save_pretrained(model_storage_dir + '/' + parameter_dict['filename'])
-        tokenizer.save_pretrained(model_storage_dir+'/'+parameter_dict['filename'])
-        trained_model.config.save_pretrained(model_storage_dir+'/'+parameter_dict['filename'])
-
-        #saving torch stuff - see torch docs for proper loading
-        torch.save(optimizer.state_dict(), Path(model_path)/Path(parameter_dict['filename']+' optimizer'))
-        torch.save(scheduler.state_dict(), Path(model_path)/Path(parameter_dict['filename']+' scheduler'))
-
-        #saving parameter dict
-        with open(results_path/'parameters.json', 'w') as jsonFile:
-            json.dump(parameter_dict, jsonFile)
-
-        np.savetxt(results_path/'loss_data', loss_data, delimiter = ',')
-
-        #plotting
-        plt.clf()
-        plt.scatter(range(parameter_dict['epochs']), loss_data)
-        plt.savefig(results_dir + '/' + parameter_dict['filename'] +'/'+'loss_plot.png')
 
 if __name__ == '__main__':
-    if (train_type == 'prepend' or train_type == 'keyword'):
-        train_batch_of_models(test_preprocessor, parameter_dict, type = 'keyword')
-    elif (train_type == 'encode_decode'):
-        train_batch_of_bert_models(test_preprocessor, parameter_dict, type = 'encode_decode')
+    train_batch_of_models(test_preprocessor, parameter_dict, type = 'encode_decode')
