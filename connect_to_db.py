82b359dfd1b783a0d81b152dbe8cd931f8bfba6c
dylan <djweber3@asu.edu>
2020-07-06 12:55:29 -0700

    cleaning shitty laptop

--- connect_to_db.py
+++ connect_to_db.py
@@ -2,7 +2,7 @@ import psycopg2
 
 def get_db_connection():
    psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
-   conn = psycopg2.connect(host='artislg-dev.cqt472ritvtr.us-east-2.rds.amazonaws.com', port='5432', user='dylan_weber',
+   conn = psycopg2.connect(host='artislg-dev.cqt472ritvtr.us-east-2.rds.amazonaws.com', port='5432', dbname = 'postgres', user='dylan_weber',
                            password='cvg0bf1DHC&Hvj%X%7!NmSt0WY0XlG')
    return conn
 
