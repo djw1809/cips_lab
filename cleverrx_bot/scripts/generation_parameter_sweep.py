2eaf7bff37249da01cdc084f4225366d6b5e410e
dylan <djweber3@asu.edu>
2020-07-23 22:15:43 -0700

    new generation sweep

--- cleverrx_bot/scripts/generation_parameter_sweep.py
+++ cleverrx_bot/scripts/generation_parameter_sweep.py
@@ -26,8 +26,8 @@ model_dict = {model_name1:model1, model_name2:model2, model_name3:model3}
 k_list = [0,20,40,60,80,100,120,140,160,180,200]
 p_list = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1]
 length = 30
-prompt1 = ['insurance-', 'Diabetes is']
-prompt2 = ['card+', 'Use']
+prompt1 = ['insurance-', 'Diabetes can be']
+prompt2 = ['card+', 'Look into']
 
 
 for key in model_dict.keys():
