5ab8c1db6412bb17e6f4b502e351f08cc58e3906
dylan <djweber3@asu.edu>
2020-07-30 19:39:38 -0700

    cleaning up

--- cleverrx_bot/scripts/cleverrx_test.py
+++ cleverrx_bot/scripts/cleverrx_test.py
@@ -11,7 +11,7 @@ import facebook_data_processing as fb
 
 #%%get data
 data_path = 'facebookCommenting-master/data/1595553517145.json'
-keep_list_path = 'facebookCommenting-master/data/072320_evening_keep_list.pkl'
+keep_list_path = 'facebookCommenting-master/data/072420_morning_keep_list.pkl'
 
 # with open(data_path, 'rb') as file:
 #     data = json.load(file)
@@ -24,11 +24,10 @@ with open(keep_list_path, 'rb') as file:
 #%%automatically select things to keep using targets
 
 ##%%onstruct comment dict
-comment_dict = fb.create_comment_dict(keep_list)
-comment_dict
+with open('facebookCommenting-master/data/comment_dict_morning_072420.pkl', 'rb') as file:
+    comment_dict = pickle.load(file)
 ##generate comment salvo and save it
 salvo = fb.add_comments(keep_list, comment_dict)
 
-
-with open('evening_072320_salvo.json', 'w') as file:
+with open('facebookCommenting-master/data/072420_morning_salvo.json', 'w') as file:
     json.dump(salvo, file)
