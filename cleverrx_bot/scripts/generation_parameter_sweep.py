91e8c75b5b65f5980504b7eb9a14b0ef8e9d70ce
Dylan <djweber3@asu.edu>
2020-08-09 11:37:29 -0700

    new generation

--- cleverrx_bot/scripts/generation_parameter_sweep.py
+++ cleverrx_bot/scripts/generation_parameter_sweep.py
@@ -19,9 +19,9 @@ model_name3 = 'batch_051220_keyword_types_sentiment_nocluster'
 
 
 
-save_name_1 = '_080920_selfcare'
-save_name_2 = '_080920_selfcare'
-save_name_3 ='_080920_selfcare'
+save_name_1 = '_080920'
+save_name_2 = '_080920'
+save_name_3 ='_080920'
 
 
 model1 = models.GPT2Model_bagofctrl.load(model_path + model_name1)
