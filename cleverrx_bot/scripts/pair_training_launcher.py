9d0c800f61c7f2a749b67938275e876c8067c94d
Dylan Weber <djweber3@en4181828l.fulton.ad.asu.edu>
2020-09-14 16:30:08 -0700

    debugging

--- cleverrx_bot/scripts/pair_training_launcher.py
+++ cleverrx_bot/scripts/pair_training_launcher.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import torch
 import matplotlib
+import faulthandler 
+faulthandler.enable()
 import pickle
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
