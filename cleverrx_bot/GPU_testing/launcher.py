8232b23f2ae1fa242e46ef8ba02ccf9f9779db12
Dylan Weber <djweber3@en4181828l.fulton.ad.asu.edu>
2020-10-07 14:43:14 -0700

    debug

--- cleverrx_bot/GPU_testing/launcher.py
+++ cleverrx_bot/GPU_testing/launcher.py
@@ -13,7 +13,7 @@ from pathlib import Path
 import torch.nn as nn
 #import bot_models as models
 import utils as butils
-from utils import Comment_data_preprocessor, Comment_dataset, Comment_pair_dataset
+from utils import  Comment_pair_dataset
 
 
 test = False
