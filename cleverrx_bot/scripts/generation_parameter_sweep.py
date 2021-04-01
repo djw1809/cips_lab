20786086fbc58a7962e4341b70f7be7f62cf480b
Dylan Weber <djweber3@en4181828l.fulton.ad.asu.edu>
2021-04-01 15:23:27 -0700

    new paramter sweep

--- cleverrx_bot/scripts/generation_parameter_sweep.py
+++ cleverrx_bot/scripts/generation_parameter_sweep.py
@@ -12,16 +12,16 @@ import bot_models as models
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
 model_path = '../saved_models/'
-
-model_name1 = 'batch_051220_keyword_types_sentiment_cluster'
-model_name2 = 'batch_051220_keyword_types_nosentiment_cluster'
+# 
+# model_name1 = 'batch_051220_keyword_types_sentiment_cluster'
+# model_name2 = 'batch_051220_keyword_types_nosentiment_cluster'
 model_name3 = 'batch_051220_keyword_types_sentiment_nocluster'
 
 
 
 #save_name_1 = 'test'
 # save_name_2 = '_081120_insurance'
-save_name_3 ='_031120_insurance'
+save_name_3 ='_040121_clever'
 
 
 #model1 = models.GPT2Model_bagofctrl.load(model_path + model_name1)
@@ -30,17 +30,18 @@ model3 = models.GPT2Model_bagofctrl.load(model_path + model_name3)
 
 model_dict = {model_name3 + save_name_3:model3} #model_name2+save_name_2:model2, model_name3+save_name_3:model3}
 
-k_list = [60,80,100,120,140,160,180,200]
-p_list = [.3, .4, .5, .6, .7, .8, .9, 1]
+k_list = [100,120,140,160,180,200]
+p_list = [.3, .4, .5, .6, .7]
 length = 50
-num_return_sequences = 20
+num_return_sequences = 30
 temperature = 1.3
 repetition_penalty = 2.5
 prompts = [(['insurance-'], 'insurance is'),
-           (['card+'], 'try'),
-           (['card+'], 'purchase'),
+           (['card+'], 'people need'),
+           (['card+'], 'this could help'),
            (['insurance-'], 'health costs'),
-           (['card+'], 'prescription costs')]
+           (['insurance-'], 'prescription costs'),
+            (['insurance-'], 'the insurance industry')]
 
 
 for key in model_dict.keys():
